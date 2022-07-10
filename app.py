27459dab710761a87d897cb9d83fb5ccc56d44a3
yassbarona <yasserbarona@gmail.com>
2022-07-10 23:00:55 +0200

    cheat sheet

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import copy
 import glob
 import pytz
 
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Sanitize working directory
@@ -31,7 +32,7 @@ st.text('Work hard... Play hard')
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh()
 #select sidebar
-add_selectbox = st.sidebar.selectbox("Select model",("68 lanmarks", "468 lanmarks"))
+add_selectbox = st.sidebar.selectbox("Select model",("68 lanmarks", "468 lanmarks", "Cheat sheet"))
 initial_iamge = './data/image3.png'
 landmark_points_68 = [162,234,93,58,172,136,149,148,152,377,378,365,397,288,323,454,389,71,63,105,66,107,336,
                 296,334,293,301,168,197,5,4,75,97,2,326,305,33,160,158,133,153,144,362,385,387,263,373,
@@ -39,6 +40,22 @@ landmark_points_68 = [162,234,93,58,172,136,149,148,152,377,378,365,397,288,323,
 if add_selectbox == "468 lanmarks":   
     landmark_points_68 = list(range(0, 468))
     initial_iamge = './data/image2.png'
+
+index_dict = {
+    "Contours": [0, 7, 10, 13, 14, 17, 21, 33, 37, 39, 40, 46, 52, 53, 54, 55, 58, 61, 63, 65, 66, 67, 70, 78, 80, 81, 82, 84, 87, 88, 91, 93, 95, 103, 105, 107, 109, 127, 132, 133, 136, 144, 145, 146, 148, 149, 150, 152, 153, 154, 155, 157, 158, 159, 160, 161, 162, 163, 172, 173, 176, 178, 181, 185, 191, 234, 246, 249, 251, 263, 267, 269, 270, 276, 282, 283, 284, 285, 288, 291, 293, 295, 296, 297, 300, 308, 310, 311, 312, 314, 317, 318, 321, 323, 324, 332, 334, 336, 338, 356, 361, 362, 365, 373, 374, 375, 377, 378, 379, 380, 381, 382, 384, 385, 386, 387, 388, 389, 390, 397, 398, 400, 402, 405, 409, 415, 454, 466], 
+    "Oval Face": [132, 389, 136, 10, 397, 400, 148, 149, 150, 21, 152, 284, 288, 162, 297, 172, 176, 54, 58, 323, 67, 454, 332, 338, 93, 356, 103, 361, 234, 109, 365, 379, 377, 378, 251, 127], 
+    "Eyes": [384, 385, 386, 387, 388, 390, 263, 362, 398, 466, 373, 374, 249, 380, 381, 382, 160, 33, 161, 163, 133, 7, 173, 144, 145, 246, 153, 154, 155, 157, 158, 159],
+    "Lips": [0, 267, 269, 270, 13, 14, 17, 402, 146, 405, 409, 415, 291, 37, 39, 40, 178, 308, 181, 310, 311, 312, 185, 314, 317, 318, 61, 191, 321, 324, 78, 80, 81, 82, 84, 87, 88, 91, 95, 375], 
+    "Eye Brows": [293, 295, 296, 300, 334, 336, 276, 282, 283, 285, 65, 66, 70, 105, 107, 46, 52, 53, 55, 63], 
+    "Irises": [474, 475, 476, 477, 472, 469, 470, 471], 
+    "Left Eye": [384, 385, 386, 387, 388, 390, 263, 362, 398, 466, 373, 374, 249, 380, 381, 382], 
+    "Left Eyebrow": [293, 295, 296, 300, 334, 336, 276, 282, 283, 285], 
+    "Left Iris": [474, 475, 476, 477], 
+    "Right Eye": [160, 33, 161, 163, 133, 7, 173, 144, 145, 246, 153, 154, 155, 157, 158, 159], 
+    "Right Eyebrow": [65, 66, 70, 105, 107, 46, 52, 53, 55, 63], 
+    "Right Iris": [472, 469, 470, 471]
+    }
+
 
 def remove_index(i_drop_str, d_indexes):
     i_drop_l = [int(i.strip()) for i in i_drop_str.split(',') if i != ''] 
@@ -102,6 +119,13 @@ def download_callback(dest_path):
 
 st.subheader('Step 1: Upload video')
 data = st.file_uploader("Upload a video", type=None, accept_multiple_files=True)
+
+if add_selectbox ==  "Cheat sheet":
+    selection = st.selectbox('Find indeces', ('Contours', 'Oval Face','Eyes','Lips','Eye Brows', 'Irises', 'Left Eye', 'Left Eyebrow','Left Iris','Right Eye', 'Right Eyebrow', 'Right Iris'))
+    for key, value in index_dict.items():
+        if selection == key:
+            print(value)
+            st.subheader(f'{value}')
 
 if len(data) >= 1 :
     with st.container():
@@ -182,13 +206,12 @@ if len(data) >= 1 :
                     zip.write(file)
             with open(f'{dest_path}.zip', 'rb') as zip:
                 st.download_button(
-                    label="Download videos as .zip",
+                    label="Download files as .zip",
                     file_name=f'{dest_path}.zip',
                     data=zip,
                     on_click=download_callback,
                     args=(dest_path,),
-                    mime='application/zip'
-                    )
+                    mime='application/zip')
         
                 
                 
